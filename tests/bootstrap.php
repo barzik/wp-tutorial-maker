476079d9004374415a8b79383e9939204848fde5
Ran Bar-Zik <ran@bar-zik.com>
2015-08-29 18:54:44 +0300

    feature/adding-travis-ci-support
    Changing the bootstrap according to https://github.com/Seravo/wordpress-test-template/blob/master/tests/bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,23 +1,25 @@
 <?php
-/**
- * Bootstrap the plugin unit testing environment.
- *
- * Edit 'active_plugins' setting below to point to your main plugin file.
- *
- * @package wordpress-plugin-tests
- */
+
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( ! $_tests_dir ) {
+	$_tests_dir = '/tmp/wordpress-tests-lib';
+}
+
+define('PLUGIN_NAME','wp-tutorial-maker.php');
+define('PLUGIN_FOLDER',basename(dirname( __DIR__ )));
+define('PLUGIN_PATH',PLUGIN_FOLDER.'/'.PLUGIN_NAME);
 
 // Activates this plugin in WordPress so it can be tested.
 $GLOBALS['wp_tests_options'] = array(
-	'active_plugins' => array( 'wp-tutorial-maker/wp-tutorial-maker.php' ),
+	'active_plugins' => array( PLUGIN_PATH ),
 );
 
-// If the develop repo location is defined (as WP_DEVELOP_DIR), use that
-// location. Otherwise, we'll just assume that this plugin is installed in a
-// WordPress develop SVN checkout.
+require_once $_tests_dir . '/includes/functions.php';
 
-if( false !== getenv( 'WP_TESTS_DIR' ) ) {
-	require getenv( 'WP_TESTS_DIR' ) . '/tests/phpunit/includes/bootstrap.php';
-} else {
-	require '/tmp/wordpress-tests-lib/tests/phpunit/includes/bootstrap.php';
+function _manually_load_plugin() {
+	require dirname( __DIR__ ) . '/'.PLUGIN_NAME;
 }
+
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+require $_tests_dir . '/includes/bootstrap.php';
