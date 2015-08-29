b85ae54a2eeebf947f386d660487000176810287
Ran Bar-Zik <ran@bar-zik.com>
2015-08-29 18:51:41 +0300

    feature/adding-travis-ci-support
    Adding bootstrap fix to support travis CI

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -16,8 +16,8 @@ $GLOBALS['wp_tests_options'] = array(
 // location. Otherwise, we'll just assume that this plugin is installed in a
 // WordPress develop SVN checkout.
 
-if( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
-	require getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit/includes/bootstrap.php';
+if( false !== getenv( 'WP_TESTS_DIR' ) ) {
+	require getenv( 'WP_TESTS_DIR' ) . '/tests/phpunit/includes/bootstrap.php';
 } else {
-	require '../../../../tests/phpunit/includes/bootstrap.php';
+	require '/tmp/wordpress-tests-lib/tests/phpunit/includes/bootstrap.php';
 }
